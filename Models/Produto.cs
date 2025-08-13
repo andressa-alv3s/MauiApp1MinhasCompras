6e539ea966ea197312a44540c8f788dc4228a404
andre <andre@DESKTOP-1PTS1RO>
2025-08-13 12:31:09 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MauiApp1MinhasCompras.Models
+{
+    public class Produto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao { get; set; } = string.Empty;
+        public double Quantidade { get; set; }
+        public double Preco { get; set; }
+
+    }
+}
