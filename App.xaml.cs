fe32f1095aabcc8e1bbf2b3fb0943f47c7d9ee0f
andre <andre@DESKTOP-1PTS1RO>
2025-08-19 17:22:38 -0300

    Definindo o camino para o banco de dados e inserindo o primeiro registro.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,15 +1,30 @@
-﻿namespace MauiApp1MinhasCompras
+﻿using MauiApp1MinhasCompras.Helpers;
+
+namespace MauiApp1MinhasCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper? _db; // Variável estática para armazenar a instância do banco de dados
+        public static SQLiteDatabaseHelper Db // Propriedade de leitura para acessar o banco de dados
+        {
+            get
+            {
+                if (_db == null) // verifica se a instância do banco de dados já foi criada
+                {
+                    string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "banco_sqlite_compras.db3"); // Define o caminho do arquivo do banco de dados
+                    _db = new SQLiteDatabaseHelper(path); // Inicializa a instância do banco de dados se ainda não estiver criada
+                }
+                return _db;
+            }
+        }
         public App()
         {
             InitializeComponent();
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
+        protected override Window CreateWindow(IActivationState? activationState) // Método para criar a janela principal da aplicação
         {
-            return new Window(new NavigationPage(new Views.ListaProduto()));
+            return new Window(new NavigationPage(new Views.ListaProduto())); // MainPage = new NavigationPage(new Views.ListaProduto());
         }
     }
 }
--- Views/NovoProduto.xaml.cs
+++ Views/NovoProduto.xaml.cs
@@ -1,3 +1,5 @@
+using MauiApp1MinhasCompras.Models;
+
 namespace MauiApp1MinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +8,29 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Produto p = new Produto
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text),
+            };
+            await App.Db.Insert(p);
+            await DisplayAlert("Sucesso", "Produto adicionado com sucesso!", "Ok");
+        }
+        catch (Exception ex)
+        {
+            _ = DisplayAlert("Ops", ex.Message, "Ok");
+
+        }
+    }
+
+    private  async void Voltar_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
 }
