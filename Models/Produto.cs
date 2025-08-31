b5ea48fc948a7475e4f194ff0adfd3d20d1632c9
andre <andre@DESKTOP-1PTS1RO>
2025-08-31 10:45:28 -0300

    Atualização, campos vazios.

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -4,10 +4,34 @@ namespace MauiApp1MinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+        double _quantidade;
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; } = string.Empty;
-        public double Quantidade { get; set; }
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("A descrição é obrigatória!");
+                }
+                _descricao = value;
+            }
+        }
+        public double Quantidade
+        {
+            get => _quantidade;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("A quantidade é obrigatória!");
+                }
+                _quantidade = value;
+            }
+        }
         public double Preco { get; set; }
         public double Total { get => Quantidade * Preco; }
     }
